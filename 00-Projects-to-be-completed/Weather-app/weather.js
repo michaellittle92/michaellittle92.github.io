6582a4742d19ffc65bb37fd7b5598874d7cd002f
michaellittle92 <michaellittle92@gmail.com>
2024-06-17 22:18:28 +1000

    weather project started.

---
+++ 00-Projects-to-be-completed/Weather-app/weather.js
@@ -0,0 +1,22 @@
+
+
+let apiKey = '';
+let lat = -37; 
+let lon = 140;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+async function init(){
+    const currentTemp = await getCurrentTemp();
+    console.log(currentTemp + '°');
+}
+
+async function getCurrentTemp(){
+    const res = await fetch(apiUrl);
+    const resObj = await res.json();
+    return convertKelvinToCelsius(resObj.main.temp);
+}
+
+function convertKelvinToCelsius(tempKelvin){
+    return tempKelvin -273.15;
+}
+init();
