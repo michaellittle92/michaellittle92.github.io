e6a15eee846747bfd856ffbebaaf8e29bae23e05
michaellittle92 <michaellittle92@gmail.com>
2024-07-01 21:36:03 +1000

    basic logic for weekly view.

--- 00-Projects-to-be-completed/Weather-app/weather.js
+++ 00-Projects-to-be-completed/Weather-app/weather.js
@@ -1,16 +1,41 @@
 async function Init(){
+    let location = 'Melbourne';
+    await getWeather(location);
+}
+
+async function getWeather( location){
+    const res = await fetch(buildDailyForcastApiString(location, apiKey));
+    const resObj = await res.json();  
+    console.log(resObj);
+    let weather = [resObj.forecast.forecastday[0].day.maxtemp_c, resObj.forecast.forecastday[0].day.mintemp_c, resObj.forecast.forecastday[0].day.condition.text];
+    
     let minTemp = document.getElementById('min-temp');
     let maxTemp = document.getElementById('max-temp');
     let weatherIcon = document.getElementById("weather-icon");
-    let date = getDate();
-    let location = 'Melbourne';
 
-    let todayWeather = (await getTodayWeather(date, location));
-    maxTemp.innerText = todayWeather[0];
-    minTemp.innerText = todayWeather[1];
-    if (todayWeather[2].toLowerCase().includes('rain')){
-        weatherIcon.src = "images/rainy-3.svg";
+    maxTemp.innerText = weather[0];
+    minTemp.innerText = weather[1];
+    //weekly forcast - should probably be its own function
+    for(let i = 0; i <= resObj.forecast.forecastday.length-1; i++){
+        dayDate = document.getElementById(`forecast-day-${i}-date`);
+        dayDate.innerText = resObj.forecast.forecastday[i].date; 
+        //forecastImage = console.log(resObj.forecast.forecastday[i].day.condition.text);
+         //TODO imliment image assignment method - should use the static version of the nicer images 
+        forecastImage = document.getElementById(`forecast-day-${i}-image`);
+        forecastImage.src = resObj.forecast.forecastday[i].day.condition.icon
         
+        forcastMaxTemp = document.getElementById(`forecast-day-${i}-maxtemp`);
+        forcastMaxTemp.innerText = resObj.forecast.forecastday[i].day.maxtemp_c;
+        forcastMinTemp = document.getElementById(`forecast-day-${i}-mintemp`);
+        forcastMinTemp.innerText = resObj.forecast.forecastday[i].day.mintemp_c;
+    }
+    //image selection should probably be its own function, one for animated (main) and one static (weekly view)
+    if(weather[2].toLowerCase() === 'patchy rain nearby'){
+        weatherIcon.src = "images/rainy-1.svg";
+    }
+    else if (weather[2].toLowerCase().includes('rain')){
+        weatherIcon.src = "images/rainy-3.svg";
+    
     }
     else{
         weatherIcon.src = "images/cloudy-1-day.svg";
@@ -18,30 +43,8 @@ async function Init(){
 
 }
 
-async function getTodayWeather(date, location){
-    const res = await fetch(buildDailyForcastApiString(date, location, apiKey));
-    const resObj = await res.json();  
-    let weather = [resObj.forecast.forecastday[0].day.maxtemp_c, resObj.forecast.forecastday[0].day.mintemp_c, resObj.forecast.forecastday[0].day.condition.text];
-    return weather
-}
-
-function getDate(){
-    let today = new Date();
-    let yyyy = today.getFullYear();
-    let mm = today.getMonth() + 1;
-    let dd = today.getDate();
-    if (mm < 10){
-        mm = '0' + mm;
-    }
-    if (dd < 10){
-        dd = '0' + dd;
-    }
-    let date = `${yyyy}-${mm}-${dd}`;
-    return date;
-}
-
-function buildDailyForcastApiString(date, location, apiKey){
-let apiString = `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=1&dt=${date}&key=${apiKey}`
+function buildDailyForcastApiString(location, apiKey){
+let apiString = `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=7&key=${apiKey}`
 return apiString;
 }
 
