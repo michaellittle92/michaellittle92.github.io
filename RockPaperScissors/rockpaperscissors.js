852da3e640b09bbef92c8edfb9e6bc4e6d692910
Michael Little <michaellittle92@gmail.com>
2025-01-25 16:29:34 +1100

    Rock Paper Scissors
    Still need to add styling. Flexbox in place.

---
+++ RockPaperScissors/rockpaperscissors.js
@@ -0,0 +1,90 @@
+const buttons = document.querySelectorAll(".game-buttons");
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    ButtonSwitch(buttons[i].id);
+  });
+}
+
+function ButtonSwitch(input) {
+  switch (input) {
+    case "rock":
+      Rock();
+      break;
+    case "paper":
+      Paper();
+      break;
+    case "scissors":
+      Scissors();
+      break;
+    case "reset":
+      reset();
+      break;
+    default:
+      console.log("an error has occoured");
+  }
+}
+
+function Rock() {
+  UpdateImage("player", "Rock");
+  PlayGame("Rock");
+}
+
+function Paper() {
+  UpdateImage("player", "Paper");
+  PlayGame("Paper");
+}
+
+function Scissors() {
+  UpdateImage("player", "Scissors");
+  PlayGame("Scissors");
+}
+
+function reset() {
+  const playerScore = document.querySelector(".player-score");
+  const cpuScore = document.querySelector(".cpu-score");
+
+  playerScore.textContent = 0;
+  cpuScore.textContent = 0;
+
+  UpdateImage("player", "empty");
+  UpdateImage("cpu", "empty");
+}
+
+function UpdateImage(user, choice) {
+  if (user == "player") {
+    document.querySelector(
+      ".player-image"
+    ).src = `/images/${choice.toLowerCase()}-250x250.png`;
+  } else {
+    document.querySelector(
+      ".cpu-image"
+    ).src = `/images/${choice.toLowerCase()}-250x250.png`;
+  }
+}
+
+function PlayGame(playerChoice) {
+  const playerScore = document.querySelector(".player-score");
+  const cpuScore = document.querySelector(".cpu-score");
+  const status = document.querySelector(".status");
+
+  const availableChoices = ["Rock", "Paper", "Scissors"];
+
+  const cpuChoice = availableChoices[Math.floor(Math.random() * 3)];
+  UpdateImage("CPU", cpuChoice);
+
+  if (playerChoice === cpuChoice) {
+    status.textContent = "Draw";
+  } else if (
+    (playerChoice === "Rock" && cpuChoice === "Scissors") ||
+    (playerChoice === "Paper" && cpuChoice === "Rock") ||
+    (playerChoice === "Scissors" && cpuChoice === "Paper")
+  ) {
+    status.textContent = "Win";
+    playerScore.textContent = (Number(playerScore.textContent) + 1).toString();
+  } else {
+    status.textContent = "Lose";
+
+    cpuScore.textContent = (Number(cpuScore.textContent) + 1).toString();
+  }
+}
